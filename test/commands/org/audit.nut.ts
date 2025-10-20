52cad9bc2b97d002e134162db4b646a8daee63b5
Jannis Schreiber <jannis.schreiber@allvest.de>
2025-10-20 17:06:52 +0200

    refactor: e2e NUT with actual entities

--- test/commands/org/audit.nut.ts
+++ test/commands/org/audit.nut.ts
@@ -4,6 +4,8 @@ import { assert, expect } from 'chai';
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { OrgAuditInitResult } from '../../../src/commands/org/audit/init.js';
 import { OrgAuditRunResult } from '../../../src/commands/org/audit/run.js';
+import { PermissionRiskLevelPresets } from '../../../src/libs/policies/types.js';
+import AuditConfigFileManager from '../../../src/libs/config/audit-run/auditConfigFileManager.js';
 
 const scratchOrgAlias = 'TestTargetOrg';
 const testingWorkingDir = path.join('test', 'mocks', 'test-sfdx-project');
@@ -12,8 +14,31 @@ describe('org audit NUTs', () => {
   let session: TestSession;
 
   function checkFileExists(filePath: string): boolean {
-    const localFilePath = path.join(session.dir, 'test-sfdx-project', filePath);
+    const localFilePath = resolveTestDirFilePath(filePath);
     return fs.existsSync(localFilePath);
+  }
+
+  function resolveTestDirFilePath(filePath: string) {
+    return path.join(session.dir, 'test-sfdx-project', filePath);
+  }
+
+  function activatePolicies(dirPath: string, preset: PermissionRiskLevelPresets) {
+    const fileManager = new AuditConfigFileManager();
+    const configDirPath = resolveTestDirFilePath(dirPath);
+    const conf = fileManager.parse(configDirPath);
+    if (conf.policies.Profiles?.content.profiles) {
+      Object.values(conf.policies.Profiles.content.profiles).forEach((profile) => {
+        // eslint-disable-next-line no-param-reassign
+        profile.preset = preset;
+      });
+    }
+    if (conf.policies.PermissionSets?.content.permissionSets) {
+      Object.values(conf.policies.PermissionSets.content.permissionSets).forEach((permSet) => {
+        // eslint-disable-next-line no-param-reassign
+        permSet.preset = preset;
+      });
+    }
+    fileManager.save(configDirPath, conf);
   }
 
   before(async () => {
@@ -71,5 +96,24 @@ describe('org audit NUTs', () => {
     assert.isDefined(result.auditDate);
     assert.isDefined(result.orgId);
     assert.isDefined(result.policies);
+  });
+
+  it('successfully completes an audit with all policies active', async () => {
+    // Arrange
+    activatePolicies('tmp', PermissionRiskLevelPresets.ADMIN);
+
+    // Act
+    // relies on the config that was created from the first test
+    const result = execCmd<OrgAuditRunResult>(`org:audit:run --target-org ${scratchOrgAlias} --source-dir tmp --json`, {
+      ensureExitCode: 0,
+    }).jsonOutput?.result;
+
+    // Assert
+    assert.isDefined(result);
+    expect(Object.keys(result.policies)).to.deep.equal(['Profiles', 'PermissionSets', 'ConnectedApps']);
+    Object.entries(result.policies).forEach(([policyName, policy]) => {
+      // every policy should have at least one audited entity
+      expect(policy.auditedEntities, policyName).not.deep.equal([]);
+    });
   });
 });
