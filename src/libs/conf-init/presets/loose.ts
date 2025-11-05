9b97e036ceb5d3f26be58485c075e7ceeaf62d7a
Jannis Schreiber <jannis.schreiber@allvest.de>
2025-11-05 20:03:52 +0100

    polish: more default perms for presets

--- src/libs/conf-init/presets/loose.ts
+++ src/libs/conf-init/presets/loose.ts
@@ -6,9 +6,13 @@ export default class LoosePreset extends NonePreset {
   public constructor() {
     super({
       UseAnyApiClient: PermissionRiskLevel.HIGH,
+      ExternalClientAppAdmin: PermissionRiskLevel.HIGH,
+      ManageSandboxes: PermissionRiskLevel.HIGH,
+      ManageDevSandboxes: PermissionRiskLevel.HIGH,
       CustomizeApplication: PermissionRiskLevel.HIGH,
       ModifyMetadata: PermissionRiskLevel.HIGH,
       AuthorApex: PermissionRiskLevel.HIGH,
+      DebugApex: PermissionRiskLevel.HIGH,
       ManageAuthProviders: PermissionRiskLevel.HIGH,
       Packaging2: PermissionRiskLevel.HIGH,
       Packaging2Delete: PermissionRiskLevel.HIGH,
@@ -18,12 +22,24 @@ export default class LoosePreset extends NonePreset {
       ManageTwoFactor: PermissionRiskLevel.HIGH,
       ManageRemoteAccess: PermissionRiskLevel.HIGH,
       CanApproveUninstalledApps: PermissionRiskLevel.HIGH,
+      AssignPermissionSets: PermissionRiskLevel.HIGH,
+      ManageIpAddresses: PermissionRiskLevel.HIGH,
+      ManageSharing: PermissionRiskLevel.HIGH,
+      ManageInternalUsers: PermissionRiskLevel.HIGH,
+      ManagePasswordPolicies: PermissionRiskLevel.HIGH,
+      ManageLoginAccessPolicies: PermissionRiskLevel.HIGH,
+      ManageCustomPermissions: PermissionRiskLevel.HIGH,
+      ManageCertificates: PermissionRiskLevel.HIGH,
+      FreezeUsers: PermissionRiskLevel.MEDIUM,
+      ManageRoles: PermissionRiskLevel.MEDIUM,
       ViewSetup: PermissionRiskLevel.MEDIUM,
       ViewAllData: PermissionRiskLevel.MEDIUM,
       ModifyAllData: PermissionRiskLevel.MEDIUM,
       ExportReport: PermissionRiskLevel.MEDIUM,
       EmailMass: PermissionRiskLevel.MEDIUM,
+      AccessContentBuilder: PermissionRiskLevel.MEDIUM,
       ApiEnabled: PermissionRiskLevel.LOW,
+      LightningExperienceUser: PermissionRiskLevel.LOW,
     });
   }
 
