7fa966161fd2e09e5e98530b0dcfba5eab565309
Jannis Schreiber <info@lietzau-consulting.de>
2025-11-26 20:57:13 +0800

    refactor: move user resolve to repo

--- src/libs/core/registries/rules/enforcePermissionsOnUser.ts
+++ src/libs/core/registries/rules/enforcePermissionsOnUser.ts
@@ -1,17 +1,8 @@
-import { buildPermsetAssignmentsQuery } from '../../constants.js';
-import MDAPI from '../../mdapi/mdapiRetriever.js';
-import { PermissionSetAssignment } from '../../policies/salesforceStandardTypes.js';
-import { isNullish } from '../../utils.js';
-import { PartialProfileLike, scanProfileLike, ScanResult } from '../helpers/permissionsScanning.js';
-import { AuditContext, PartialPolicyRuleResult, RuleAuditContext } from '../types.js';
-import { ResolvedUser, UserPermissionSetAssignment } from '../users.js';
+import UsersRepository, { PermissionSetAssignment } from '../../mdapi/usersRepository.js';
+import { scanProfileLike, ScanResult } from '../helpers/permissionsScanning.js';
+import { PartialPolicyRuleResult, RuleAuditContext } from '../types.js';
+import { ResolvedUser } from '../users.js';
 import PolicyRule, { RuleOptions } from './policyRule.js';
-
-type UserPermSetAssignments = Record<string, UserPermissionSetAssignment[]>;
-
-type ResolvedAssignment = UserPermissionSetAssignment & {
-  metadata?: PartialProfileLike;
-};
 
 export default class EnforcePermissionsOnUser extends PolicyRule<ResolvedUser> {
   public constructor(opts: RuleOptions) {
@@ -21,32 +12,30 @@ export default class EnforcePermissionsOnUser extends PolicyRule<ResolvedUser> {
   public async run(context: RuleAuditContext<ResolvedUser>): Promise<PartialPolicyRuleResult> {
     const result = this.initResult();
     const users = context.resolvedEntities;
-    const assignments = await fetchAssignments(context, Object.values(users));
-    const mdapiRepo = MDAPI.create(context.targetOrgConnection);
-    const permsets = await mdapiRepo.resolve('PermissionSet', uniquePermSetNames(assignments));
-    const profiles = await mdapiRepo.resolve('Profile', uniqueProfileNames(Object.values(users)));
+    const userRepo = new UsersRepository(context.targetOrgConnection);
+    const userPerms = await userRepo.resolveUserPermissions(Object.values(users));
     for (const user of Object.values(users)) {
-      const resolvedPermsetAssignments = assignments[user.userId]
-        ? assignments[user.userId].map((ass) => ({
-            ...ass,
-            metadata: permsets[ass.permissionSetIdentifier],
-          }))
-        : [];
-      const permsetResult = this.scanAssignedPermissionSets(user, resolvedPermsetAssignments);
+      const resolvedPerms = userPerms.get(user.userId);
+      if (!resolvedPerms) {
+        continue;
+      }
+      const permsetResult = this.scanAssignedPermissionSets(user, resolvedPerms.assignedPermissionsets);
       result.violations.push(...permsetResult.violations);
       result.warnings.push(...permsetResult.warnings);
-      const profileResult = scanProfileLike(
-        { preset: user.role, metadata: profiles[user.profileName], name: user.profileName },
-        this.auditContext,
-        [user.username]
-      );
-      result.violations.push(...profileResult.violations);
-      result.warnings.push(...profileResult.warnings);
+      if (resolvedPerms.profileMetadata) {
+        const profileResult = scanProfileLike(
+          { preset: user.role, metadata: resolvedPerms.profileMetadata, name: user.profileName },
+          this.auditContext,
+          [user.username]
+        );
+        result.violations.push(...profileResult.violations);
+        result.warnings.push(...profileResult.warnings);
+      }
     }
     return result;
   }
 
-  private scanAssignedPermissionSets(user: ResolvedUser, actualAssignments: ResolvedAssignment[]): ScanResult {
+  private scanAssignedPermissionSets(user: ResolvedUser, actualAssignments: PermissionSetAssignment[]): ScanResult {
     const result: ScanResult = { violations: [], warnings: [] };
     for (const assignedPermSet of actualAssignments) {
       if (!assignedPermSet.metadata) {
@@ -62,40 +51,4 @@ export default class EnforcePermissionsOnUser extends PolicyRule<ResolvedUser> {
     }
     return result;
   }
-}
-
-async function fetchAssignments(context: AuditContext, users: ResolvedUser[]): Promise<UserPermSetAssignments> {
-  const permSetAssignments: Awaited<ReturnType<typeof fetchAssignments>> = {};
-  const assignments = await context.targetOrgConnection.query<PermissionSetAssignment>(
-    buildPermsetAssignmentsQuery(users.map((u) => u.userId))
-  );
-  for (const assignment of assignments.records) {
-    if (isNullish(permSetAssignments[assignment.AssigneeId])) {
-      permSetAssignments[assignment.AssigneeId] = [];
-    }
-    permSetAssignments[assignment.AssigneeId].push({
-      permissionSetIdentifier: assignment.PermissionSet.Name,
-      permissionSetSource: assignment.PermissionSetGroupId ? 'group' : 'direct',
-      groupName: assignment.PermissionSetGroup?.DeveloperName,
-    });
-  }
-  return permSetAssignments;
-}
-
-function uniquePermSetNames(assignments: UserPermSetAssignments): string[] {
-  const uniquePermSets = new Set<string>();
-  for (const assList of Object.values(assignments)) {
-    for (const ass of assList) {
-      uniquePermSets.add(ass.permissionSetIdentifier);
-    }
-  }
-  return Array.from(uniquePermSets);
-}
-
-function uniqueProfileNames(users: ResolvedUser[]): string[] {
-  const uniqueProfiles = new Set<string>();
-  for (const usr of users) {
-    uniqueProfiles.add(usr.profileName);
-  }
-  return Array.from(uniqueProfiles);
 }
