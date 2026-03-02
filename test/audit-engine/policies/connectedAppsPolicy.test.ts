edb10db1ed4a7f27378e0406f4af0c33dab51745
Jannis Schreiber <jannis.schreiber@allvest.de>
2026-03-02 08:51:44 +0100

    feat: add app users to violation details

--- test/audit-engine/policies/connectedAppsPolicy.test.ts
+++ test/audit-engine/policies/connectedAppsPolicy.test.ts
@@ -33,66 +33,94 @@ describe('policy - connected apps', () => {
     $$.reset();
   });
 
-  it('resolves all apps from ConnectedApplication and OauthToken', async () => {
-    // Arrange
-    $$.mocks.mockOAuthTokens('oauth-usage');
+  describe('entity resolve', () => {
+    it('resolves all apps from ConnectedApplication and OauthToken', async () => {
+      // Arrange
+      $$.mocks.mockOAuthTokens('oauth-usage');
 
-    // Act
-    const policyResult = await resolveAndRun('connectedApps', $$);
+      // Act
+      const policyResult = await resolveAndRun('connectedApps', $$);
 
-    // Assert
-    expect(policyResult.ignoredEntities).to.deep.equal([]);
-    expect(policyResult.auditedEntities).to.deep.equal([
-      'Chatter Desktop',
-      'Salesforce for Android',
-      'Chatter Mobile for BlackBerry',
-      'Salesforce for iOS',
-      'Test App 1',
-      'Test App 2',
-      'AI Platform Auth',
-    ]);
-    expect(policyResult.isCompliant).to.equal(true);
-    const executedRuleNames = Object.keys(policyResult.executedRules);
-    expect(executedRuleNames).to.deep.equal([]);
+      // Assert
+      expect(policyResult.ignoredEntities).to.deep.equal([]);
+      expect(policyResult.auditedEntities).to.deep.equal([
+        'Chatter Desktop',
+        'Salesforce for Android',
+        'Chatter Mobile for BlackBerry',
+        'Salesforce for iOS',
+        'Test App 1',
+        'Test App 2',
+        'AI Platform Auth',
+      ]);
+      expect(policyResult.isCompliant).to.equal(true);
+      const executedRuleNames = Object.keys(policyResult.executedRules);
+      expect(executedRuleNames).to.deep.equal([]);
+    });
+
+    it('uses result form ApiAccess setting to override self-authorize flag', async () => {
+      // Arrange
+      await $$.mocks.stubMetadataRetrieve('security-settings');
+      defaultConfig.rules.NoUserCanSelfAuthorize.enabled = true;
+
+      // Act
+      const policyResult = await resolveAndRun('connectedApps', $$);
+
+      // Assert
+      expect(policyResult.ignoredEntities).to.deep.equal([]);
+      expect(policyResult.auditedEntities).to.deep.equal([
+        'Chatter Desktop',
+        'Salesforce for Android',
+        'Chatter Mobile for BlackBerry',
+        'Salesforce for iOS',
+        'Test App 1',
+      ]);
+      expect(policyResult.isCompliant).to.equal(true);
+      const executedRuleNames = Object.keys(policyResult.executedRules);
+      expect(executedRuleNames).to.deep.equal(['NoUserCanSelfAuthorize']);
+    });
+
+    it('gracefully handles if ApiAccess setting is not available on org', async () => {
+      // Arrange
+      await $$.mocks.stubMetadataRetrieve('api-access-not-available');
+      defaultConfig.rules.NoUserCanSelfAuthorize.enabled = true;
+
+      // Act
+      const pol = loadPolicy('connectedApps', $$.mockAuditConfig);
+      const resolveResult = await pol.resolve({ targetOrgConnection: $$.targetOrgConnection });
+
+      // Assert
+      expect(resolveResult.ignoredEntities).to.deep.equal([]);
+      expect(Object.keys(resolveResult.resolvedEntities)).to.have.lengthOf(5);
+      Object.values(resolveResult.resolvedEntities).forEach((appConf) => {
+        const resolvedApp = appConf;
+        expect(resolvedApp.overrideByApiSecurityAccess).to.be.false;
+      });
+    });
   });
 
-  it('uses result form ApiAccess setting to override self-authorize flag', async () => {
-    // Arrange
-    await $$.mocks.stubMetadataRetrieve('security-settings');
-    defaultConfig.rules.NoUserCanSelfAuthorize.enabled = true;
+  describe('rule execution', () => {
+    describe('AllUsedAppsUnderManagement', () => {
+      it('adds each user of an app to details of violation', async () => {
+        // Arrange
+        $$.mocks.mockOAuthTokens('oauth-usage');
 
-    // Act
-    const policyResult = await resolveAndRun('connectedApps', $$);
+        // Act
+        defaultConfig.rules.AllUsedAppsUnderManagement.enabled = true;
+        const policyResult = await resolveAndRun('connectedApps', $$);
 
-    // Assert
-    expect(policyResult.ignoredEntities).to.deep.equal([]);
-    expect(policyResult.auditedEntities).to.deep.equal([
-      'Chatter Desktop',
-      'Salesforce for Android',
-      'Chatter Mobile for BlackBerry',
-      'Salesforce for iOS',
-      'Test App 1',
-    ]);
-    expect(policyResult.isCompliant).to.equal(true);
-    const executedRuleNames = Object.keys(policyResult.executedRules);
-    expect(executedRuleNames).to.deep.equal(['NoUserCanSelfAuthorize']);
-  });
-
-  it('gracefully handles if ApiAccess setting is not available on org', async () => {
-    // Arrange
-    await $$.mocks.stubMetadataRetrieve('api-access-not-available');
-    defaultConfig.rules.NoUserCanSelfAuthorize.enabled = true;
-
-    // Act
-    const pol = loadPolicy('connectedApps', $$.mockAuditConfig);
-    const resolveResult = await pol.resolve({ targetOrgConnection: $$.targetOrgConnection });
-
-    // Assert
-    expect(resolveResult.ignoredEntities).to.deep.equal([]);
-    expect(Object.keys(resolveResult.resolvedEntities)).to.have.lengthOf(5);
-    Object.values(resolveResult.resolvedEntities).forEach((appConf) => {
-      const resolvedApp = appConf;
-      expect(resolvedApp.overrideByApiSecurityAccess).to.be.false;
+        // Assert
+        const ruleResult = policyResult.executedRules.AllUsedAppsUnderManagement;
+        expect(ruleResult.isCompliant).to.be.false;
+        expect(ruleResult.violations).to.have.lengthOf(2);
+        expect(ruleResult.violations.at(0)).to.deep.contain({
+          identifier: ['Test App 2'],
+          details: ['test3@example.com'],
+        });
+        expect(ruleResult.violations.at(1)).to.deep.contain({
+          identifier: ['AI Platform Auth'],
+          details: ['cloud@00d3x000001tal2uaa'],
+        });
+      });
     });
   });
 });
